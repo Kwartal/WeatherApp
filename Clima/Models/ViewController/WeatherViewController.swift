ff1c3c43af09d91b384b24ae1b3090dbe21fb694
kwartal <bv.barinov@gmail.com>
2022-09-12 12:10:43 +0300

    fix lables

--- Clima/Models/ViewController/WeatherViewController.swift
+++ Clima/Models/ViewController/WeatherViewController.swift
@@ -21,8 +21,13 @@ class WeatherViewController: UIViewController {
     
     var networkWeatherManager = NetworkWeatherManager()
     
-    let locationManager = CLLocationManager()
-    
+    lazy var locationManager: CLLocationManager = {
+        let lm = CLLocationManager()
+        lm.delegate = self
+        lm.desiredAccuracy = kCLLocationAccuracyKilometer
+        lm.requestWhenInUseAuthorization()
+        return lm
+    }()
     
     // MARK: - Life Cycle
     override func viewDidLoad() {
@@ -34,6 +39,11 @@ class WeatherViewController: UIViewController {
             
             print(currentWeather.cityName)
         }
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.requestLocation()
+        }
+        
         networkWeatherManager.delegate = self
         
         backgroundSettings()
@@ -80,7 +90,7 @@ extension WeatherViewController {
         cityLabel.adjustsFontSizeToFitWidth = true
         
         feelsLikeTemperatureLabel.text = "Feelslike " + "25" + "℃"
-        feelsLikeTemperatureLabel.font = .systemFont(ofSize: 25, weight: .regular)
+        feelsLikeTemperatureLabel.font = .systemFont(ofSize: 18, weight: .regular)
         feelsLikeTemperatureLabel.adjustsFontSizeToFitWidth = true
         feelsLikeTemperatureLabel.textColor = Colors.weatherColor
         
@@ -103,24 +113,24 @@ extension WeatherViewController {
         weatherStatusImage.snp.makeConstraints {
             $0.top.equalTo(searchButton.snp.bottom).offset(10)
             $0.trailing.equalToSuperview().inset(30)
-            $0.size.equalTo(150)
+            $0.size.equalTo(200)
         }
         
         temperatureLabel.snp.makeConstraints {
             $0.top.equalTo(weatherStatusImage.snp.bottom).offset(5)
             $0.trailing.equalToSuperview().inset(16)
-            $0.size.equalTo(120)
+            $0.size.equalTo(100)
         }
         
         
         feelsLikeTemperatureLabel.snp.makeConstraints {
-            $0.top.equalTo(temperatureLabel.snp.bottom).offset(5)
+            $0.top.equalTo(temperatureLabel.snp.bottom)
             $0.trailing.equalTo(temperatureLabel)
         }
         
         cityLabel.snp.makeConstraints {
-            $0.top.equalTo(feelsLikeTemperatureLabel.snp.bottom).offset(10)
-            $0.trailing.equalToSuperview().inset(40)
+            $0.top.equalTo(searchButton)
+            $0.trailing.equalTo(searchButton.snp.leading).inset(5)
             $0.height.equalTo(30)
         }
         
@@ -136,7 +146,7 @@ extension WeatherViewController {
     
     @objc private func searchButtonPressed() {
         presentSearchAlertController(with: "Enter city name", message: nil, style: .alert) { [unowned self] city in
-            self.networkWeatherManager.fetchCurrentWeather(for: city)
+            self.networkWeatherManager.fetchCurrentWeather(forRequestTupe: .cityName(city: city))
         }
     }
     
@@ -154,6 +164,23 @@ extension WeatherViewController {
 extension WeatherViewController: NetworkWeatherManagerDelegate {
     func updateInterface(_: NetworkWeatherManager, with currentWeather: CurrentWeather) {
         print(currentWeather.cityName)
+    }
+}
+
+// MARK: - CLLoacationManagerDelegate
+
+extension WeatherViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        let latitude = location.coordinate.latitude
+        let longitude = location.coordinate.longitude
+        
+        networkWeatherManager.fetchCurrentWeather(forRequestTupe: .coordinate(latitude: latitude, longitude: longitude))
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
     }
 }
 
